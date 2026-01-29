1c9e3407440e0ae3c8db8ca8316209b284da0ddd
Oussema Bouzidi <bouzidioussema16@gmail.com>
2026-01-29 06:30:48 +0100

    Fix failing MySQL FK migration

--- backend/prisma/migrations/20260129005626_/migration.sql
+++ backend/prisma/migrations/20260129005626_/migration.sql
@@ -1,3 +1,33 @@
+-- DropForeignKey
+ALTER TABLE `favorites` DROP FOREIGN KEY `favorites_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `favorites` DROP FOREIGN KEY `favorites_userId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `menu_products` DROP FOREIGN KEY `menu_products_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `menu_products` DROP FOREIGN KEY `menu_products_menuId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `menu_services` DROP FOREIGN KEY `menu_services_serviceId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `menu_services` DROP FOREIGN KEY `menu_services_menuId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `order_items` DROP FOREIGN KEY `order_items_orderId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `order_items` DROP FOREIGN KEY `order_items_productId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `orders` DROP FOREIGN KEY `orders_serviceId_fkey`;
+
+-- DropForeignKey
+ALTER TABLE `orders` DROP FOREIGN KEY `orders_userId_fkey`;
+
 -- DropIndex
 DROP INDEX `favorites_productId_fkey` ON `favorites`;
 
